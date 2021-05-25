c7022bd8b80a00582aaf2f196451438834ac27a5
ngoclike <nhuntn97@gmail.com>
2021-05-26 00:22:58 +0700

    sua 1 chut cho pow pheechj

--- Session5/app.js
+++ Session5/app.js
@@ -8,16 +8,14 @@ function checkString(myStr) {
 function findMaxDiff(myInter) {
     if (myInter.length < 2) {
         return 0;
-    } else {
-        let maxDiff = 0;
-        for (let i = 0; i < myInter.length; i++) {
-            let a = Math.abs(myInter[i] - myInter[i -1]);
-            if (maxDiff < a) {
-                maxDiff = a;
-            }
-        }
-        return maxDiff;
     }
+    let maxDiff = 0;
+    for (let i = 0; i < myInter.length - 1; i++) {
+        if (Math.abs(myInter[i] - myInter[i + 1]) > maxDiff) {
+            maxDiff = Math.abs(myInter[i] - myInter[i + 1]);
+        }
+    }
+    return maxDiff;
 }
 // console.log(findMaxDiff([1, -10, 5, 18, -9, 5]));
 
@@ -25,54 +23,52 @@ function findMaxDiff(myInter) {
 function findMaxDiff(myInter) {
     if (myInter.length < 2) {
         return 0;
-    } else {
-        let maxDiff = 0;
-        for (let i = 0; i < myInter.length; i++) {
-            maxDiff = maxDiff < Math.abs(myInter[i] - myInter[i -1])  ? maxDiff = Math.abs(myInter[i] - myInter[i -1]) : maxDiff;
-        }
-        return maxDiff;
     }
+    let maxDiff = 0;
+    for (let i = 1; i < myInter.length; i++) {
+        maxDiff = maxDiff < Math.abs(myInter[i] - myInter[i - 1])  ? maxDiff = Math.abs(myInter[i] - myInter[i - 1]) : maxDiff;
+    }
+    return maxDiff;
 }
-// console.log(findMaxDiff([1, -10, 5, 18, -9, 5]));
+//console.log(findMaxDiff([1, -10, 5, 18, -9, 5]));
 
 // 3.
 function firstValue(arr, n) {
-    if (n <= 0) {
+    if (n <= 0)
         return arr = [];
-    } else if (n > arr.lenght) {
+    
+    if (n > arr.lenght) 
         return arr;
-    } else {
-        let firstArr = [];
-        for (let index = 0; index < arr.length; index++) {
-            if (index < n) {
-                firstArr.push(arr[index]);
-            }
+
+    let firstArr = [];
+    for (let index = 0; index < arr.length; index++) {
+        if (index < n) {
+            firstArr.push(arr[index]);
         }
-        return firstArr;
     }
+    return firstArr;
+
+    // return arr.slice(0, n);
 }
-// console.log(firstValue([10, 3, -6, 22, 9, -41, 56], 8));
+// console.log(firstValue([10, 3, -6, 22, 9, -41, 56], 2));
 
 // 4.
 function checkInt (number1, number2) {
-    if (number1 > 0 &&  number2 < 0 || number1 < 0 && number2 > 0 || number1 > 0 && number2 > 0) {
+    if (number1 * number2 < 0)
         return true;
-    } else if (number1 == 0 || number2 == 0) {
-        return 0;
-    } else {
-        return false;
-    }
+
+    return false;
 }
-// console.log(checkInt(-1, 6));
+// console.log(checkInt(1, -6));
 
 // 5.
 function exercise5(n) {
     let difference = Math.abs(n - 13);
-    if (n > 13) {
+    if (n > 13) 
         return difference *= 2;
-    } else {
-        return difference;
-    }
+
+    return difference;
+    
 }
 // console.log(exercise5(20));
 
@@ -80,17 +76,14 @@ function exercise5(n) {
 
 function findMax(arr) {
     //arr.sort();
-    let max = 0;
+    let maxDifference = 0;
     for (let i = 0; i < arr.length -1; i++) {
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                let tempt = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tempt;
+            if (Math.abs(arr[i] - arr[j]) > maxDifference) {
+                maxDifference = Math.abs(arr[i] - arr[j]);
             }
         }
     }
-    let difference = Math.abs(arr[arr.length - 1] - arr[0])
-    return difference;
+    return maxDifference;
 }
-// console.log(findMax([7, 4, -1, 15, 2]));
+//console.log(findMax([7, 4, -1, 15, 2]));
